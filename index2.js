4e83baa41365efdafd8814d10460e09c8589c24d
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-06-22 16:23:13 +0530

    added time stamp in index2

--- index2.js
+++ index2.js
@@ -2,7 +2,14 @@
 var redis = require('redis')
 var client = redis.createClient()
 var fs = require('fs')
+var os = require('os');
 
-client.eval(fs.readFileSync('./debug_script.lua'), 4, 1, 'score', 0, 10, 'meeting_type:email', 'meeting_status:open', function(err, res) {
-  console.log(res);
-});
+console.time("The time");
+for(var i = 0; i < 10; ++i)
+{
+    client.eval(fs.readFileSync('./debug_script.lua'), 4, 1, 'score', 0, 10, 'meeting_type:email', 'meeting_status:open', function(err, res) {
+      console.log(res);
+      console.timeEnd("The time");
+    });
+}
+
