89f1ef0792e25aa53f637e03c538d7830b1772fe
Amit Upadhyay <youarefreak1@gmail.com>
2017-06-23 11:04:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,19 +13,3 @@ client.on('connect', function() {
 
 
 console.log('222');
-
-var Scripto = require('redis-scripto');
-
-console.log('333');
-var scriptManager = new Scripto(client);
-
-console.log('444');
-scriptManager.loadFromDir('./scripts/');
-console.log('555');
-
-var keys    = [3, 'score', 0, 10];
-var values  = ['meeting_type:email', 'meeting_status:close'];
-scriptManager.run('debug_script.lua', keys, values, function(err, result) {
-    console.log("something happened : "+ err);
-});
-
